7f3e3d0e917cfc0dc92b6827ed72d14bfeecb0cb
VictorCabrera-Tera <56519850+VictorCabrera-Tera@users.noreply.github.com>
2022-04-07 04:47:56 -0400

    basic pause interface

--- game.cpp
+++ game.cpp
@@ -1,6 +1,6 @@
-#define is_down(b) input->buttons[b].is_down
-#define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
-#define released(b) (!(input->buttons[b].is_down) && input->buttons[b].changed)
+//#define is_down(b) input->buttons[b].is_down
+//#define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
+//#define released(b) (!(input->buttons[b].is_down) && input->buttons[b].changed)
 
 
 #include "game_global_variables.cpp"
